5321426681ea63bd06bcb8ae336ae914461e0490
stevebiczyk <stevebiczyk@gmail.com>
2024-01-16 23:13:47 +0000

    Renders & Re-renders in React

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log("1");
   }
 
   componentDidMount() {
+    console.log("3");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -19,13 +21,14 @@ class App extends Component {
             return { monsters: users };
           },
           () => {
-            console.log(this.state.monsters);
+            console.log(this.state);
           }
         )
       );
   }
 
   render() {
+    console.log("2");
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
