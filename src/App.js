d66d0b6dc89953a21dbde46fb263d1ebfb43734b
stevebiczyk <stevebiczyk@gmail.com>
2024-01-17 22:59:55 +0000

    added search box component

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
-    console.log("1");
+    console.log("constructor");
   }
 
   componentDidMount() {
-    console.log("3");
+    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -28,9 +28,17 @@ class App extends Component {
   }
 
   render() {
-    console.log("2");
+    console.log("render");
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
