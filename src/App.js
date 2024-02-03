167143cd4ddb504c631fcafb6b9784b4f3161dcc
stevebiczyk <stevebiczyk@gmail.com>
2024-02-03 18:29:55 +0000

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchBox from "./components/search-box/search-box.component";
 import CardList from "./components/card-list/card-list.component";
 import "./App.css";
 
 const App = () => {
-  // const [monsters, setMonsters] = useState([]);
   const [searchField, setSearchField] = useState("");
-  console.log(searchField);
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
-
-    // this.setState(() => {
-    //   return { searchField };
-    // });
   };
+
   return (
     <div className="App">
       <h1 className="app-title"> Monsters Rolodex </h1>
@@ -24,7 +34,7 @@ const App = () => {
         onChangeHandler={onSearchChange}
         placeholder="search monsters"
       />
-      {/* <CardList monsters={filteredMonsters} /> */}
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 };
@@ -59,10 +69,6 @@ const App = () => {
 //   render() {
 //     const { monsters, searchField } = this.state;
 //     const { onSearchChange } = this;
-
-//     const filteredMonsters = monsters.filter((monster) => {
-//       return monster.name.toLocaleLowerCase().includes(searchField);
-//     });
 
 //     return (
 
