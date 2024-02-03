7b28c10fa288781e7d0146c325052a17ce606cb1
stevebiczyk <stevebiczyk@gmail.com>
2024-02-03 19:23:30 +0000

    converted remaining componets

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,20 +1,18 @@
-import { Component } from "react";
 import "./card.styles.css";
 
-class Card extends Component {
-  render() {
-    const { name, email, id } = this.props.monster;
-    return (
-      <div className="card-container" key={id}>
-        <img
-          src={`https://robohash.org/${id}?set=set2&size=180x180`}
-          alt={`monster ${name}`}
-        />
-        <h2> {name} </h2>
-        <p> {email} </p>
-      </div>
-    );
-  }
-}
+const Card = ({ monster: { id, name, email } }) => {
+  // Destructure the properties from monster object
+
+  return (
+    <div className="card-container" key={id}>
+      <img
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+        alt={`monster ${name}`}
+      />
+      <h2> {name} </h2>
+      <p> {email} </p>
+    </div>
+  );
+};
 
 export default Card;
